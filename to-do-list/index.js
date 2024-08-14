c15cf91f10f8e8ddb971e92eae910446b2c0cec8
yagnesh alla <yagneshalla@gmail.com>
2024-08-14 16:56:25 +0530

    first commit

---
+++ to-do-list/index.js
@@ -0,0 +1,81 @@
+let tbody = document.getElementById("cardcontainer")
+let alltodos = []
+let i = 0
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('alltodos') !== null) {
+        alltodos = JSON.parse(localStorage.getItem('alltodos'))
+    }
+    alltodos.forEach(todo => {
+        populate_data(todo)
+    })
+    if (localStorage.getItem('radionum') !== null) {
+        i = JSON.parse(localStorage.getItem('radionum')).radionum
+    }
+})
+document.getElementById("additemform").addEventListener("submit", (e) => {
+    e.preventDefault()
+    let item = document.getElementById("work").value
+    let desc = document.getElementById("description").value
+    let category = document.getElementById("category").value
+    let priority = document.getElementById("priority").value
+    let obj = {
+        item: item,
+        description: desc,
+        category: category,
+        priority: priority
+    }
+    alltodos.push(obj)
+    localStorage.setItem('alltodos', JSON.stringify(alltodos))
+
+    populate_data(obj)
+
+    document.getElementById("additemform").reset()
+})
+
+function populate_data(table_data) {
+    let trow = document.createElement("div")
+    trow.className = "card"
+    trow.innerHTML = `
+        <h4>${table_data.item}</h4>
+        <h4>${table_data.description}</h4>
+        <h4>${table_data.category}</h4>
+        <h4>${table_data.priority}</h4>`
+
+    let radios = document.createElement("div")
+    radios.className = "radios"
+    radios.innerHTML = `
+        <label for="todo">To-Do</label><input type="radio" id="todo" name=${i}>
+        <label for="progress">In Progress</label><input type="radio" id="progress" name=${i}>
+        <label for="complete">Completed</label><input type="radio" id="complete" name=${i}>
+        `
+    i++
+
+    let obj1 = { radionum: i }
+
+    localStorage.setItem('radionum', JSON.stringify(obj1))
+    let del = document.createElement("button")
+    del.innerHTML = "Delete"
+    del.className = "deleteitem"
+
+    trow.appendChild(radios)
+    trow.appendChild(del)
+
+    del.addEventListener('click', () => {
+        let closestcard = del.closest('.card')
+        closestcard.style.backgroundColor = 'red'
+        alltodos = alltodos.filter((data) => {
+                if (data !== table_data)
+                    return data
+            })
+            // alltodos.splice(alltodos.indexOf(table_data),1)
+        localStorage.setItem('alltodos', JSON.stringify(alltodos))
+        setTimeout(() => {
+            closestcard.remove()
+        }, 300);
+
+    })
+
+    tbody.appendChild(trow)
+
+
+}
